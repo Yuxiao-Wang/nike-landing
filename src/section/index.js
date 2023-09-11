a54245093e91fbc489baca26842b5366be0e222e
Yuxiao-Wang <aquarius1994@vip.qq.com>
2023-09-11 16:20:58 +0800

    Add files via upload

---
+++ src/section/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import SuperQuality from "./SuperQuality";
+import Services from "./Service.jsx";
+import CustomerReviews from "./CustomerReviews";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
+import PopularProduct from "./PopularProduct";
+import SpecialOffer from "./SpecialOffer";
+
+export {
+  Hero,
+  PopularProduct,
+  SuperQuality,
+  Services,
+  CustomerReviews,
+  Subscribe,
+  Footer,
+  SpecialOffer,
+};
